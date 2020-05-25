3ccc6f574527a5ea4960ffb14a5362eb3bd36128
knowhere1998 <darekar.amey@gmail.com>
2020-05-25 16:24:57 +0530

    Added network config

--- client.py
+++ client.py
@@ -12,6 +12,21 @@ ENDC = '\033[m'
 ip_addr = '127.0.0.1'
 secondary_ip = False
 
+# Node identity
+node_identity = 1 
+
+def get_neighbours(identity):
+	file = open('network_config.txt', 'r')
+	matrices = file.readlines()
+	items = matrices[node_identity].rstrip('\n').split(', ')
+	neighbours = [ True if item == "True" else item for item in items ]
+	neighbours = [ False if item == "False" else item for item in neighbours ]
+	for index, item in enumerate(neighbours):
+		print(item, end=' ' if index != 0 else '\n')
+	print()
+	return neighbours
+
+
 def ip4_addresses():
 	ip_list = []
 	global secondary_ip
@@ -19,12 +34,10 @@ def ip4_addresses():
 	for interface in interfaces():
 		for addr_fam, link in ifaddresses(interface).items():
 			if addr_fam == AF_INET and 'docker' not in interface and 'lo' not in interface:
-				print(interface, link[0])
 				address = {'interface': interface, 'address': link[0]['addr'], 'netmask': link[0]['netmask']}
 				if interface == 'wlp6s0' or interface == 'eth0':
 					ip_addr = link[0]['addr']
 				if interface ==	'wlp6s0:1' or interface ==	'eth0:1':
-					print(interface)
 					secondary_ip = True
 	return ip_list
 
@@ -51,11 +64,13 @@ def connect():
 	print("connected!")
 	hostname = socket.gethostname()
 	address_list = ip4_addresses()
-	print(uuid.getnode())
+	neighbours = get_neighbours(node_identity)
 	data = {
 		"mac": uuid.getnode(),
 		"pid": os.getpid(),
 		"system": hostname,
+		"node_identity": node_identity,
+		"neighbours": neighbours,
 		"primary_ip": ip_addr,
 		"secondary_ip": secondary_ip,
 		"additional_network_info": address_list
@@ -77,11 +92,11 @@ def reconnect():
 	sock.connect('http://localhost:5000')
 
 if __name__ == '__main__':
+	node_identity = int(input("Enter Node identity: \t")[0])
 	try:
 		while True:
 			if not sock.connected:
 				reconnect()
-				print('connected with sid: ', sock.sid)
 			else:
 				ping = input("Enter message for server: \t")
 				if ping != 'disconnect':
