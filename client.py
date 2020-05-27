7c6324703c9c4bb6d6cb64c065dcc490d2deb3e8
knowhere1998 <darekar.amey@gmail.com>
2020-05-27 23:40:00 +0530

    Deleted config.py and implemented loggers.

--- client.py
+++ client.py
@@ -1,21 +1,42 @@
-import socketio
-import time, datetime, os, socket, uuid
+import socketio, time, datetime, os, socket, uuid, logging, sys
 from netifaces import interfaces, ifaddresses, AF_INET
 
 from timeloop import Timeloop
 from datetime import timedelta, datetime
 
-LOGINPASSWD = os.environ.get('PASSWORD', False)
-NODE_ID = int(os.environ.get('NODE_ID', False))
-
 tl = Timeloop()
 sock = socketio.Client()
+LOGINPASSWD = os.environ.get('PASSWORD', False)
+NODE_ID = int(os.environ.get('NODE_ID', False))
+secondary_ip = False
+primary_ip = None
+
+logger = logging.getLogger('operations')
+fileHandler = logging.FileHandler('operations.log')
+formatter = logging.Formatter('[%(asctime)s] - [%(name)s] - [%(levelname)s] - %(message)s')
+fileHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
+logger.setLevel(logging.DEBUG)
+
+pingLogger = logging.getLogger('ping')
+pingFileHandler = logging.FileHandler('ping.log')
+pingFileHandler.setFormatter(formatter)
+pingLogger.addHandler(pingFileHandler)
+pingLogger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler() 			# Console Log Handler
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+pingLogger.addHandler(ch)
 
 
 @tl.job(interval=timedelta(seconds=2))
-def ping_beat():
+def pingLeat():
 	if sock.connected:
 		now = datetime.now()
+		pingLogger = logging.getLogger('ping')
+		pingLogger.debug("Sent alive ping")
 		message('ping', now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-2])
 
 
@@ -25,12 +46,9 @@ def get_neighbors(identity):
 	items = matrices[NODE_ID].rstrip('\n').split(', ')
 	neighbors = [ True if item == "True" else item for item in items ]
 	neighbors = [ False if item == "False" else item for item in neighbors ]
-	print("Self ID: ", neighbors[0])
-	print("All possible neighbors are: ")
-	for neighbor in neighbors[1:]:
-		print(neighbor, end=' ')
-	print()
+	logger.info("Self ID: %s", NODE_ID)
 	neighbors.pop(0)
+	logger.info("All possible neighbors are: %s", neighbors)
 	return neighbors
 
 
@@ -79,30 +97,30 @@ def await_reconnection_command():
 
 @sock.on('recover')
 def on_message(data):
-	print("Received recovery request:", data)
-	print("Recovering Node "+ str(data['disconnected_node']) + "...")
-	print(TLOAD + "creating virtual IP address: " + data['ip'], ENDC)
+	logger.info("Received recovery request:", data)
+	logger.info("Recovering Node "+ str(data['disconnected_node']) + "...")
+	logger.info("creating virtual IP address: " + data['ip'])
 	if sys.platform.startswith('win'):
-		os.system("ifconfig wlp6s0:1 192.168.0.150 netmask 255.255.255.0 up")
+		os.system("ifconfig wlp6s0:1 " + data['ip'] + " netmask 255.255.255.0 up")
 	elif sys.platform.startswith('lin'):
 		if not LOGINPASSWD:
-			print(TRED + "Environment variable not Initialized. Unable to recover", ENDC)
+			logger.warning("Environment variable not Initialized. Unable to recover")
 			message('update node', data)
 			return
-		os.system("echo " + LOGINPASSWD + " | sudo -S ifconfig wlp6s0:1 192.168.0.150 netmask 255.255.255.0 up")
+		os.system("echo " + LOGINPASSWD + " | sudo -S ifconfig wlp6s0:1 " + data['ip'] + " netmask 255.255.255.0 up")
 	else:
-		print(TRED + "Unable to determine the System os. Unable to recover", ENDC)
+		logger.warning("Unable to determine the System os. Unable to recover")
 		message('update node', data)
 		return
 	ip_addresses = get_ip4_addresses()
-	print(ip_addresses)
-	print(secondary_ip)
+	logger.info(ip_addresses)
+	logger.info(secondary_ip)
 	if secondary_ip:
-		print(TGREEN + "Success! Notifying Server...", ENDC)
+		logger.info("Success! Notifying Server...")
 		updated_data = populate_server_info(ip_addresses)
 		message('update node', updated_data)
 	else:
-		print(TRED + "unable to recover node", ENDC)
+		logger.warning("unable to recover node")
 		message('update node', data)
 
 
@@ -111,42 +129,48 @@ def connect():
 	ip_addresses = get_ip4_addresses()
 	data = populate_server_info(ip_addresses)
 	message('join', data)
-	print("connected!")
+	logger.info("connected!")
 
 
 @sock.event
 def connect_error():
-	print("The connection failed!")
-	print("trying to recconnect.")
+	logger.critical("The connection failed!")
+	logger.warning("trying to recconnect.")
 	await_reconnection_command()
 
 @sock.event
 def disconnect():
-	tl.stop()
-	print("I'm disconnected!")
+	logger.warning("I'm disconnected!")
 
 def reconnect():
 	time.sleep(2)
 	sock.connect('http://localhost:5000')
-	tl.start()
 
 if __name__ == '__main__':
+	tl.start()
+	logger = logging.getLogger('operations')
+	pingLogger = logging.getLogger('ping')
+	logger.critical("-------------------------------Program Execution Started-------------------------------")
 	try:
-		print(TLOAD+"initializing Node identity, gathering IP addresses and connecting to Failover Server", ENDC)
+		logger.info("initializing Node identity, gathering IP addresses and connecting to Failover Server")
 		if NODE_ID is False:
-			print(TRED+ "ERROR! Could not load Node identity", ENDC)
-			print("Initialize the Node identity with environment variable 'NODE_ID'. Look at README.txt for more info.")
+			logger.critical("ERROR! Could not load Node identity")
+			logger.warning("Initialize the Node identity with environment variable 'NODE_ID'. Look at README.txt for more info.")
+			logger.critical("-----------------------------------Program terminated-----------------------------------")
+			tl.stop()
 			exit()
 		while True:
 			if not sock.connected:
 				reconnect()
 			else:
-				ping = input("type in disconnect to terminate the session: \t")
+				ping = input("type in disconnect to terminate the session: \n")
 				if ping == 'disconnect':
 					sock.disconnect()
 					await_reconnection_command()
 	except KeyboardInterrupt:
+		logger.info("KeyboardInterrupt occured.")
+		logger.warning("Disconnecting!")
+		logger.info('Session Interrupted by User. Program terminated')
+		logger.critical("-----------------------------------Program terminated-----------------------------------")
 		tl.stop()
-		print(TLOAD + "KeyboardInterrupt occured.")
-		print(TRED + "Disconnecting!", ENDC)
 		exit()
