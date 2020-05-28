b7df4b316fee7bdf17fc4e7a67641667cf1f7f12
knowhere1998 <darekar.amey@gmail.com>
2020-05-28 23:02:21 +0530

    Fixed secondary netmask issue

--- client.py
+++ client.py
@@ -8,8 +8,11 @@ tl = Timeloop()
 sock = socketio.Client()
 LOGINPASSWD = os.environ.get('PASSWORD', False)
 NODE_ID = int(os.environ.get('NODE_ID', False))
-secondary_ip = False
+secondary_ip = None
+ip_interface = None
 primary_ip = None
+primary_netmask = None
+secondary_netmask = None
 
 logging.getLogger('timeloop').setLevel(logging.ERROR)
 
@@ -54,37 +57,38 @@ def get_neighbors(identity):
 	return neighbors
 
 
-def populate_server_info(ip_addresses):
+def populate_server_info():
 	hostname = socket.gethostname()
-	address_list = ip_addresses
 	neighbors = get_neighbors(NODE_ID)
 	data = {
 		"system": hostname,
 		"NODE_ID": NODE_ID,
 		"neighbors": neighbors,
 		"primary_ip": ip_addr,
+		"primary_netmask": primary_netmask,
 		"secondary_ip": secondary_ip,
-		"additional_network_info": address_list
+		"secondary_netmask": secondary_netmask,
 	}
 	return data
 
 
 def get_ip4_addresses():
-	ip_list = []
 	global secondary_ip
 	global ip_addr
-	secondary_ip = False
+	global ip_interface
+	global primary_netmask
+	global secondary_netmask
+	secondary_ip = None
 	for interface in interfaces():
 		for addr_fam, link in ifaddresses(interface).items():
 			if addr_fam == AF_INET and 'docker' not in interface and 'lo' not in interface:
-				address = {'address': link[0]['addr'], 'netmask': link[0]['netmask']}
 				if interface == 'wlp6s0' or interface == 'eth0':
 					ip_addr = link[0]['addr']
-					ip_list.append({interface: address})
+					primary_netmask = link[0]['netmask']
+					ip_interface = interface + ':1'
 				if interface ==	'wlp6s0:1' or interface ==	'eth0:1':
-					ip_list.append({interface: address})
 					secondary_ip = link[0]['addr']
-	return ip_list
+					secondary_netmask = link[0]['netmask']
 
 
 def message(event, data):
@@ -100,35 +104,65 @@ def await_reconnection_command():
 
 @sock.on('recover')
 def on_message(data):
-	logger.info("Received recovery request:", data)
-	logger.info("Recovering Node "+ str(data['disconnected_node']) + "...")
+	logger.info("Received recovery request. Recovering node "+ str(data['disconnected_node']) + "...")
 	logger.info("creating virtual IP address: " + data['ip'])
 	if sys.platform.startswith('win'):
-		os.system("ifconfig wlp6s0:1 " + data['ip'] + " netmask 255.255.255.0 up")
+		os.system("ifconfig " + ip_interface + data['ip'] + "netmask " + data['netmask'] + " up")
 	elif sys.platform.startswith('lin'):
 		if not LOGINPASSWD:
 			logger.warning("Environment variable not Initialized. Unable to recover")
 			message('update node', data)
 			return
-		os.system("echo " + LOGINPASSWD + " | sudo -S ifconfig wlp6s0:1 " + data['ip'] + " netmask 255.255.255.0 up")
+		os.system("echo " + LOGINPASSWD + " | sudo -S ifconfig " + ip_interface + " 192.168.0.150 netmask" + data['netmask'] + " up")
 	else:
 		logger.warning("Unable to determine the System os. Unable to recover")
 		message('update node', data)
 		return
-	ip_addresses = get_ip4_addresses()
+	get_ip4_addresses()
 	if secondary_ip:
 		logger.info("Successfully created secondary IP as %s Notifying Server...", secondary_ip)
-		updated_data = populate_server_info(ip_addresses)
+		updated_data = populate_server_info()
+		updated_data.update({'disconnected_node': data['disconnected_node']})
+		print(updated_data)
 		message('update node', updated_data)
 	else:
 		logger.warning("unable to recover node")
 		message('update node', data)
 
 
+@sock.on('restore')
+def on_message(data):
+	logger.info("Received restoration request:", data)
+	logger.info("Restoring Node "+ str(data['restore_node']) + "...")
+	logger.info("Deleting virtual IP address: " + data['ip'])
+	# if sys.platform.startswith('win'):
+	# 	os.system("ifconfig wlp6s0:1 " + data['ip'] + " up")
+	# elif sys.platform.startswith('lin'):
+	# 	if not LOGINPASSWD:
+	# 		logger.warning("Environment variable not Initialized. Unable to recover")
+	# 		message('update node', data)
+	# 		return
+	# 	os.system("echo " + LOGINPASSWD + " | sudo -S ifconfig wlp6s0:1 192.168.0.150 up")
+	# else:
+	# 	logger.warning("Unable to determine the System os. Unable to recover")
+	# 	message('update node', data)
+	# 	return
+	# ip_addresses = get_ip4_addresses()
+	# if secondary_ip:
+	# 	logger.info("Successfully created secondary IP as %s Notifying Server...", secondary_ip)
+	# 	updated_data = populate_server_info(ip_addresses)
+	# 	updated_data.update({'disconnected_node': data['disconnected_node']})
+	# 	print(updated_data)
+	# 	message('update node', updated_data)
+	# else:
+	# 	logger.warning("unable to recover node")
+	# 	message('update node', data)
+
+
 @sock.event
 def connect():
-	ip_addresses = get_ip4_addresses()
-	data = populate_server_info(ip_addresses)
+	get_ip4_addresses()
+	data = populate_server_info()
 	message('join', data)
 	logger.info("connected!")
 
